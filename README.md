5e969efe30001cf1c94a0b61c203cf3a7c834db4
xpai <xpai@users.noreply.github.com>
2023-07-13 13:34:20 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Baidu2023_CVR_Challenge
+## Baidu2023_CVR_Challenge
+
 百度商业AI技术创新大赛：商业转化行为预测
+
+
+```bash
+cd ~/Baidu2023_CVR_Challenge/data/data205411
+7zr x train_data.7z
+mv 2023-cvr-contest-data/train_data ~/Baidu2023_CVR_Challenge/data/
+cd ~/Baidu2023_CVR_Challenge/data/data204194
+7zr x test_data.7z
+mv test_data ~/Baidu2023_CVR_Challenge/data/
+
+```
+
+```bash
+cd ~/Baidu2023_CVR_Challenge/
+python data_prepare_v1.py
+```
+
+### Version 1
+```
+python run_param_tuner.py --config config/FINAL_data_v1_tuner_config_01.yaml --gpu 0
+```
+
+<div align="center">
+    <img width="80%" src="./img/submit_0.74063.png">
+</div>
+
+### Version 2
+```
+python run_param_tuner.py --config config/FINAL_data_v1_tuner_config_01.yaml --gpu 0
+```
+<div align="center">
+    <img width="80%" src="./img/submit_0.7417.png">
+</div>
--- data_prepare_v1.py
+++ data_prepare_v1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import glob
 import os
+import gc
+
 
 train_dir = "./data/train_data/"
 test_dir = "./data/test_data/"
@@ -11,10 +13,10 @@ train_files.sort()
 test_files = glob.glob(os.path.join(test_dir, "*.txt"))
 os.makedirs(output_dir, exist_ok=True)
 
-def convert_data(data_files):
-    rows = []
+def convert_data(data_files, out_file):
+    num_samples = 0
     for txt_file in data_files:
-        with open(txt_file, "r") as fd:
+        with open(txt_file, "r", encoding="utf-8") as fd:
             for line in fd:
                 rs = line.strip().split("\t")
                 log_id = rs[0]
@@ -29,29 +31,34 @@ def convert_data(data_files):
                     feat_id, field_id = fs.split(":")
                     feat_list[int(field_id) - 1].append(feat_id)
                 feat_list = ["^".join(feat) for feat in feat_list]
-                rows.append([log_id, label, label_t1, label_t2, label_t3] + feat_list)
-    return rows
+                row = [log_id, label, label_t1, label_t2, label_t3] + feat_list
+                out_file.write(",".join(row) + "\n")
+                num_samples += 1
+    return num_samples
 
-train_rows = convert_data(train_files)
-print(f"Number of samples in train_data: {len(train_rows)}")
-data = pd.DataFrame(train_rows, columns=["log_id", "label", "label_t1", "label_t2", "label_t3",
-                    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11",
-                    "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21",
-                    "f22", "f23", "f24", "f25", "f26"])
-valid_samples = 234912
-train_samples = len(train_rows) - valid_samples
-print(f"Number of training samples: {train_samples}")
-print(f"Number of validation samples: {valid_samples}")
+fout = open(os.path.join(output_dir, "data.csv"), "w", encoding="utf-8")
+headers = ["log_id", "label", "label_t1", "label_t2", "label_t3",
+            "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11",
+            "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21",
+            "f22", "f23", "f24", "f25", "f26"]
+fout.write(",".join(headers) + "\n")
+num_samples = convert_data(train_files, fout)
+fout.close()
+print(f"data.csv saved: {num_samples} samples.")
+
+data = pd.read_csv(os.path.join(output_dir, "data.csv"))
+valid_samples = 234912 # equal to testing samples
+train_samples = len(data) - valid_samples
 train_data = data.iloc[:train_samples, :]
 valid_data = data.iloc[train_samples:, :]
+del data
+gc.collect()
 train_data.to_csv(os.path.join(output_dir, "train.csv"), index=False)
 valid_data.to_csv(os.path.join(output_dir, "valid.csv"), index=False)
+print(f"train.csv saved: {train_samples} samples.")
+print(f"valid.csv saved: {valid_samples} samples.")
 
-test_rows = convert_data(test_files)
-print(f"Number of test samples: {len(test_rows)}")
-data = pd.DataFrame(test_rows, columns=["log_id", "label", "label_t1", "label_t2", "label_t3",
-                    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11",
-                    "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21",
-                    "f22", "f23", "f24", "f25", "f26"])
-data.to_csv(os.path.join(output_dir, "test.csv"), index=False)
-
+fout = open(os.path.join(output_dir, "test.csv"), "w", encoding="utf-8")
+fout.write(",".join(headers) + "\n")
+test_samples = convert_data(test_files, fout)
+print(f"test.csv saved: {test_samples} samples.")
